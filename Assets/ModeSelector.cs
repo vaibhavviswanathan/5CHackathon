c4c5e5b8aebbcc1166e3669fe1e66498165094ae
zephlord <jacob.l.rosenbloom@gmail.com>
2015-04-11 00:15:22 -0700

    Sounds and Shapes added

---
+++ Assets/ModeSelector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class ModeSelector : MonoBehaviour {
+
+	// enumerate the modes of interaction
+	const int PLAY_MODE = 0;
+	const int EDIT_MODE = 1;
+	const int LOOP_MODE = 2;
+
+	Controller controller;
+	int currentMode = 0;
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller();
+		controller.EnableGesture (Gesture.GestureType.TYPECIRCLE, true);	
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		Frame frame = controller.Frame(); //The latest frame
+		foreach (Gesture gesture in frame.Gestures())
+		{
+			switch(gesture.Type)
+			{
+			case(Gesture.GestureType.TYPECIRCLE):
+				currentMode = (currentMode + 1)%3;
+				break;
+			}
+		}
+
+
+		switch(currentMode){
+		case(PLAY_MODE):
+			break;
+		case(EDIT_MODE):
+			break;
+		case(LOOP_MODE):
+			break;
+		}	
+	}
+}
