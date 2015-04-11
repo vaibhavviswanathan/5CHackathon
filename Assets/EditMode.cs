b2a776aaa271d6492f32158c6840cf4cde5221e1
zephlord <jacob.l.rosenbloom@gmail.com>
2015-04-11 02:24:24 -0700

    Leap Issue Fixed

---
+++ Assets/EditMode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditMode : MonoBehaviour {
+
+	bool inEditMode;
+	const int EDIT_MODE = 1;
+	// Use this for initialization
+	void Start () {
+		GameObject targetObj = GameObject.Find("Main Camera"); // drag the object with the Clips variable here
+		ModeSelector targetScript = targetObj.GetComponent<ModeSelector>();
+		inEditMode = targetScript.currentMode == EDIT_MODE;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject targetObj = GameObject.Find("Main Camera"); // drag the object with the Clips variable here
+		ModeSelector targetScript = targetObj.GetComponent<ModeSelector>();
+		inEditMode = targetScript.currentMode == EDIT_MODE;	
+
+
+
+		if (inEditMode) {
+			GameObject.Find("GUI/Text").GetComponent<UnityEngine.UI.Text>().text = "Edit Mode";
+
+		}
+	
+	}
+}
---
+++ Assets/LeapRotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeapRotate : MonoBehaviour {
+
+	GameObject handController;
+	GameObject eyeAnchor;
+	float lastYRotate;
+	// Use this for initialization
+	void Start () {
+		handController = GameObject.Find ("HandController");
+		eyeAnchor = GameObject.Find ("Main Camera/TrackingSpace/CenterEyeAnchor");
+		lastYRotate = eyeAnchor.GetComponent<Transform> ().rotation.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float Yrotation = handController.GetComponent<Transform> ().rotation.y + 
+			(eyeAnchor.GetComponent<Transform> ().rotation.y - lastYRotate);
+
+		Quaternion newRotation = handController.transform.rotation;
+		newRotation [1] = Yrotation;
+		handController.transform.rotation = newRotation;
+		lastYRotate = eyeAnchor.GetComponent<Transform> ().rotation.y;
+	
+	}
+}
