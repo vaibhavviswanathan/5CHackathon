487fc0749b6fdf663ad05cb2510b82fb09889daa
zephlord <jacob.l.rosenbloom@gmail.com>
2015-04-10 21:07:56 -0700

    initial upload

---
+++ Assets/MakeShape.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeShape : MonoBehaviour {
+
+
+	ArrayList shapes;
+	// Use this for initialization
+	void Start () {
+		shapes = new ArrayList();
+	}
+
+	void makeCube(){
+		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		shapes.Add(cube);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			makeCube ();
+
+		 	
+		
+	
+	}
+}
